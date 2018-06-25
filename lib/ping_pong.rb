fa44f7c7fd5307c3a2abbae11d4dcc3ca00f0434
Craig Wann and Eric Shull <eric@>
2018-06-25 13:12:44 -0700

    refactor ping_pong_spec.rb

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,18 +1,3 @@
-# x = 10
-#
-# # number_array = []
-# # while (x <= ARVG)
-# #   x = x + 1
-# #   number_array.push(x)
-# # end
-#
-# eric_test = [1,2,3,4,5]
-#
-#   split_sentence = title.split
-#   split_sentence.each do |word|
-#     word.capitalize!()
-#   end
-#   split_sentence.join(" ")
 
 def ping_pong(number)
 
@@ -30,19 +15,5 @@ def ping_pong(number)
       input_array.push(x)
     end
   end
-
   input_array
 end
-#
-# number = gets.to_i
-#
-# puts number
-#
-# input_array = []
-# x = 0
-# while(x < number)
-#   x = x + 1
-#   input_array.push(x)
-# end
-#
-# puts input_array
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -1,35 +1,17 @@
 require('rspec')
 require('ping_pong')
 
-# describe('') do
-#   # eventually put specs here
-# end
-#
-# describe('#ping_pong') do
-#   it("capitalizes the first letter of all word in") do
-#     expect(ping_pong("the color purple")).to(eq("The Color Purple"))
-#   end
-# end
 
 describe('#ping_pong') do
   it ("input number returns integers from 1 to input number") do
   expect(ping_pong(2)).to(eq([1,2]))
   end
-end
-
-describe('#ping_pong') do
   it ("if input number is divisible by 3 returns ping") do
   expect(ping_pong(3)).to(eq([1,2,"ping"]))
   end
-end
-
-describe('#ping_pong') do
   it ("if input number is divisible by 5 returns pong") do
   expect(ping_pong(5)).to(eq([1,2,"ping",4,"pong"]))
   end
-end
-
-describe('#ping_pong') do
   it ("if input number is divisible by 15 returns pingpong") do
   expect(ping_pong(15)).to(eq([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"pingpong"]))
   end
